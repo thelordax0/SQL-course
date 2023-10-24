6d20ef9ecbe07c3f92f909b566e45eb20154403c
Mustafa Ataseven <83077139+thelordax0@users.noreply.github.com>
2023-10-24 10:57:13 +0300

    Create odev3.sql

---
+++ odev3.sql
@@ -0,0 +1,8 @@
+--1
+--select country from country where country ilike 'A%a';
+--2
+--select country from country where  country like '______%' and country like '%n';
+--3
+--select title from film where title ilike '%t%t%t%t%';
+--4
+--select * from film where title like 'C%' and length >90 and rental_rate=2.99;
